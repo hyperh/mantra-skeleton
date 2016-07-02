318530096c97730ae09c12b534d396f1731bbd57
HL <heyse.hli@gmail.com>
2016-07-02 18:53:59 +0800

    Rename to Home

--- client/modules/core/routes.js
+++ client/modules/core/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 import MainLayout from './components/MainLayout';
-import Home from './containers/Home2';
+import Home from './containers/Home';
 
 export default function (injectDeps, {Meteor, FlowRouter, Collections, LocalState}) {
   const MainLayoutCtx = injectDeps(MainLayout);
